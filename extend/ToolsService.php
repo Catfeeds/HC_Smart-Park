a885784e946b5e5e28fa6ebdd67ff3cb167ed19c
xpwsgg <xpwsgg@163.com>
2018-09-12 20:25:18 +0800

    ..

--- extend/ToolsService.php
+++ extend/ToolsService.php
@@ -10,10 +10,9 @@ class ToolsService
 {
     public static function corsOptionsHandler()
     {
-//        if (request()->isOptions()) {
+        if (request()->isOptions()) {
             header('Access-Control-Allow-Origin:*');
-            /*header('Access-Control-Allow-Headers:Accept,Referer,Host,Keep-Alive,User-Agent,X-Requested-With,Cache-Control,Content-Type,Cookie,token');*/
-            header('Access-Control-Allow-Headers:Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With');
+            header('Access-Control-Allow-Headers:Accept,Referer,Host,Keep-Alive,User-Agent,X-Requested-With,Cache-Control,Content-Type,Cookie,token,Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With');
             header('Access-Control-Allow-Credentials:true');
             header('Access-Control-Allow-Methods:GET, POST, PATCH, PUT, DELETE');
             header('Access-Control-Max-Age:1728000');
@@ -21,11 +20,11 @@ class ToolsService
             header('Content-Length: 0', true);
             header('status: 204');
             header('HTTP/1.0 204 No Content');
-        /*} else {
+        } else {
             header('Access-Control-Allow-Origin:*');
             header('Access-Control-Allow-Headers:Accept,Referer,Host,Keep-Alive,User-Agent,X-Requested-With,Cache-Control,Content-Type,Cookie,token');
             header('Access-Control-Allow-Credentials:true');
             header('Access-Control-Allow-Methods:GET,POST,OPTIONS');
-        }*/
+        }
     }
 }
