50503000398f5c06af6b90adab8f2c4fbcb19102
xpwsgg <xpwsgg@163.com>
2018-10-08 15:23:09 +0800

    apilog用户id修改为9999

--- app/api/behavior/ApiLog.php
+++ app/api/behavior/ApiLog.php
@@ -1,43 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: xpwsg
- * Date: 2018/9/27
- * Time: 9:13
- */
-
-namespace app\api\behavior;
-
-use think\Db;
-use think\Request;
-
-class ApiLog
-{
-    public function run(&$param)
-    {
-        $request = Request::instance();
-        $requestData = $request->param();
-        foreach ($requestData as &$v) {
-            if (is_string($v)) {
-                $v = mb_substr($v, 0, 200);
-            }
-        }
-        $data = [
-            'uid' => session('hid') ?: 0,
-            'ip' => $request->ip(),
-            'location' => implode(' ', \Ip::find($request->ip())),
-            'os' => getOs(),
-            'browser' => getBroswer(),
-            'url' => $request->url(),
-            'module' => $request->module(),
-            'controller' => $request->controller(),
-            'action' => $request->action(),
-            'method' => $request->isAjax() ? 'Ajax' : ($request->isPjax() ? 'Pjax' : $request->method()),
-            'data' => serialize($requestData),
-            'otime' => time(),
-        ];
-            Db::name('web_log')->insert($data);
-    }
-}
-
-;
+<?php/** * Created by PhpStorm. * User: xpwsg * Date: 2018/9/27 * Time: 9:13 */namespace app\api\behavior;use think\Db;use think\Request;class ApiLog{    public function run(&$param)    {        $request = Request::instance();        $requestData = $request->param();        foreach ($requestData as &$v) {            if (is_string($v)) {                $v = mb_substr($v, 0, 200);            }        }        $data = [            'uid' => session('hid') ?: 9999,            'ip' => $request->ip(),            'location' => implode(' ', \Ip::find($request->ip())),            'os' => getOs(),            'browser' => getBroswer(),            'url' => $request->url(),            'module' => $request->module(),            'controller' => $request->controller(),            'action' => $request->action(),            'method' => $request->isAjax() ? 'Ajax' : ($request->isPjax() ? 'Pjax' : $request->method()),            'data' => serialize($requestData),            'otime' => time(),        ];            Db::name('web_log')->insert($data);    }};
