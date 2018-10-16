a73a7509de949aa5a8ee7d4cc01d62842724b209
chen <vipchenpost@163.com>
2018-10-16 14:16:41 +0800

    导入echarts插件更换

--- public/echarts/pie-charts.js
+++ public/echarts/pie-charts.js
@@ -2,12 +2,11 @@ var dom = document.getElementById("index-pie");
 var myChart = echarts.init(dom);
 var app = {};
 option = null;
-var data = genData(50);
-
 option = {
     title : {
-        text: '二期',
-        subtext: '纯属虚构',
+        show:false,
+        text: '二期房源分布图',
+        subtext: '',
         x:'center'
     },
     tooltip : {
@@ -15,22 +14,23 @@ option = {
         formatter: "{a} <br/>{b} : {c} ({d}%)"
     },
     legend: {
-        type: 'scroll',
         orient: 'vertical',
-        right: 10,
-        top: 20,
-        bottom: 20,
-        data: data.legendData,
-
-        selected: data.selected
+        left: 'left',
+        data: ['已租','未租','已售出','已预订','房自留']
     },
     series : [
         {
-            name: '姓名',
+            name: '二期房源分布图',
             type: 'pie',
             radius : '55%',
-            center: ['40%', '50%'],
-            data: data.seriesData,
+            center: ['50%', '60%'],
+            data:[
+                {value:42, name:'已租'},
+                {value:123, name:'未租'},
+                {value:12, name:'已售出'},
+                {value:8, name:'已预订'},
+                {value:46, name:'房自留'}
+            ],
             itemStyle: {
                 emphasis: {
                     shadowBlur: 10,
@@ -41,44 +41,6 @@ option = {
         }
     ]
 };
-
-
-
-
-function genData(count) {
-    var nameList = [
-        '赵', '钱', '孙', '李', '周', '吴', '郑', '王', '冯', '陈', '褚', '卫', '蒋', '沈', '韩', '杨', '朱', '秦', '尤', '许', '何', '吕', '施', '张', '孔', '曹', '严', '华', '金', '魏', '陶', '姜', '戚', '谢', '邹', '喻', '柏', '水', '窦', '章', '云', '苏', '潘', '葛', '奚', '范', '彭', '郎', '鲁', '韦', '昌', '马', '苗', '凤', '花', '方', '俞', '任', '袁', '柳', '酆', '鲍', '史', '唐', '费', '廉', '岑', '薛', '雷', '贺', '倪', '汤', '滕', '殷', '罗', '毕', '郝', '邬', '安', '常', '乐', '于', '时', '傅', '皮', '卞', '齐', '康', '伍', '余', '元', '卜', '顾', '孟', '平', '黄', '和', '穆', '萧', '尹', '姚', '邵', '湛', '汪', '祁', '毛', '禹', '狄', '米', '贝', '明', '臧', '计', '伏', '成', '戴', '谈', '宋', '茅', '庞', '熊', '纪', '舒', '屈', '项', '祝', '董', '梁', '杜', '阮', '蓝', '闵', '席', '季', '麻', '强', '贾', '路', '娄', '危'
-    ];
-    var legendData = [];
-    var seriesData = [];
-    var selected = {};
-    for (var i = 0; i < 50; i++) {
-        name = Math.random() > 0.65
-            ? makeWord(4, 1) + '·' + makeWord(3, 0)
-            : makeWord(2, 1);
-        legendData.push(name);
-        seriesData.push({
-            name: name,
-            value: Math.round(Math.random() * 100000)
-        });
-        selected[name] = i < 6;
-    }
-
-    return {
-        legendData: legendData,
-        seriesData: seriesData,
-        selected: selected
-    };
-
-    function makeWord(max, min) {
-        var nameLen = Math.ceil(Math.random() * max + min);
-        var name = [];
-        for (var i = 0; i < nameLen; i++) {
-            name.push(nameList[Math.round(Math.random() * nameList.length - 1)]);
-        }
-        return name.join('');
-    }
-}
 ;
 if (option && typeof option === "object") {
     myChart.setOption(option, true);
